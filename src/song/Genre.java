63996c0b329f0f62ccd9e56a0f38287cf9d22b10
ScriptSonny <rhsvbo@outlook.com>
2025-03-18 14:07:25 +0100

    Implemented playback function
    added test csv
    removed json annotations

--- src/song/Genre.java
+++ src/song/Genre.java
@@ -1,18 +1,14 @@
 package song;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum Genre
 {
     POP,
     ROCK,
     METAL,
+    RAP,
     HOUSE,
     TECHNO;
 
-    @JsonCreator
     public static Genre fromString(String text) {
         for (Genre g : Genre.values()) {
             if (g.name().equalsIgnoreCase(text)) {
